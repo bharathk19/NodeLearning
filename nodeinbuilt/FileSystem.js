0644b0b5ca9341cb70a1640ad16a0fe4a3ef5021
bharathk19 <kumar.bharath289@gmail.com>
2023-01-13 00:57:18 -0500

    Adding the appending funtion to filesystems

--- nodeinbuilt/FileSystem.js
+++ nodeinbuilt/FileSystem.js
@@ -1,6 +1,23 @@
 let fs = require("fs");
 
-fs.writeFile("Main.txt", "Hello World", function (err) {
+let json = {
+  India: { city: ["Bangalore", "India"], country: ["India", "Indonesia"] },
+};
+
+/* fs.writeFile("Main.txt", "Hello World", function (err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("The file was saved!");
+}); */
+fs.appendFile("Main.txt", "Hello World", function (err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("The file was saved!");
+});
+
+fs.appendFile("Main.json", JSON.stringify(json), function (err) {
   if (err) {
     return console.log(err);
   }
