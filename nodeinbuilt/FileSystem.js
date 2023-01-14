d88d58eb8465fd3489b370c4214a9ad32b44f0e9
bharathk19 <kumar.bharath289@gmail.com>
2023-01-14 17:38:12 -0500

    Adding reading file code

--- nodeinbuilt/FileSystem.js
+++ nodeinbuilt/FileSystem.js
@@ -9,7 +9,7 @@ let json = {
     return console.log(err);
   }
   console.log("The file was saved!");
-}); */
+}); 
 fs.appendFile("Main.txt", "Hello World", function (err) {
   if (err) {
     return console.log(err);
@@ -22,4 +22,18 @@ fs.appendFile("Main.json", JSON.stringify(json), function (err) {
     return console.log(err);
   }
   console.log("The file was saved!");
+}); */
+fs.readFile("Main.txt", "utf8", function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(data);
+});
+fs.readFile("Main.json", "utf8", function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(data);
 });
